af2b3e4475e1e34b7f47e9da837f508fdaed6695
Aman50 <amanbhalla50@gmail.com>
2022-08-06 22:07:43 +0530

    Added side-navbar, modal

--- shared.js
+++ shared.js
@@ -5,17 +5,33 @@ var planSelectionBtns = document.querySelectorAll('.plan .button');
 
 var modalCloseBtn = document.querySelector('.modal .modal__action--negative');
 
+var sideNavToggleBtn = document.querySelector('.toggle-button');
+var sideNav = document.querySelector('.mobile-nav');
+
 for(var i = 0; i < planSelectionBtns.length; i++) {
     planSelectionBtns[i].addEventListener('click', function() {
-        backdrop.style.display = 'block';
-        modal.style.display = 'block';
+        backdrop.classList.add('open');
+        modal.classList.add('open');
     });
 };
 
-modalCloseBtn.addEventListener('click', closeModal);
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', () => {
+    sideNav.classList.remove('open');
+    closeModal();
+});
+
+if (modalCloseBtn) {
+    modalCloseBtn.addEventListener('click', closeModal);
+}
+
+sideNavToggleBtn.addEventListener('click', () => {
+    sideNav.classList.add('open');
+    backdrop.classList.add('open');
+});
 
 function closeModal() {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+    backdrop.classList.remove('open');
+    if (modal) {
+        modal.classList.remove('open');
+    }
 }
